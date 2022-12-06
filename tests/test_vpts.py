d1a7765ac784d72672dcbd8bc939bdcfb1ee53ab
stijnvanhoey <stijnvanhoey@gmail.com>
2022-12-06 22:41:46 +0100

    Add blueprint unit tests

---
+++ tests/test_vpts.py
@@ -0,0 +1,10 @@
+
+
+
+class TestVptsCsv():
+
+    def test_duplicate_entries():
+        """Pick only first if duplicate entries (radar, datetime and height combination) are present."""
+
+    def test_sorting():
+        """vpts data is sorted radar > timestamp > height"""
