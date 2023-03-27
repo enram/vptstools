67ce2044649a2ecfc14a31d34b7f6b4a7a64068c
stijnvanhoey <stijnvanhoey@gmail.com>
2023-03-28 00:05:24 +0200

    Add unit tests for undetec and nodata input

--- tests/test_vpts.py
+++ tests/test_vpts.py
@@ -5,11 +5,12 @@ from pathlib import Path
 import pytest
 import numpy as np
 
-from vptstools.odimh5 import ODIMReader
+from vptstools.odimh5 import ODIMReader, InvalidSourceODIM
 from vptstools.vpts import (vpts, vp, vpts_to_csv, BirdProfile,
-                            validate_vpts, DESCRIPTOR_FILENAME)
+                            validate_vpts, DESCRIPTOR_FILENAME,
+                            _convert_to_source)  #noqa
 from vptstools.vpts_csv import (int_to_nodata, datetime_to_proper8601,
-                                get_vpts_version)
+                                get_vpts_version, AbstractVptsCsv)
 from vptstools.s3 import OdimFilePath
 
 import pandas as pd
@@ -22,6 +23,7 @@ When creating a new version of the vpts specification class (VptsCsvVx), add thi
 to the pytest parameterize decorators of these test classes to apply these tests to the new version as well. All
 tests should be VptsCsv independent.
 """
+
 
 def _convert_to_source_dummy(file_path):
     """Return the file name itself from a file path"""
@@ -56,7 +58,7 @@ class TestVpts:
         """All columns are handled as str columns"""
         file_paths = sorted(path_with_vp.rglob("*.h5"))
         df_vpts = vpts(file_paths, vpts_version)
-        # check for str in data itsefl insread of 'object' on Pandas level
+        # check for str in data itself is read of 'object' on Pandas level
         assert all([isinstance(value, str) for value in df_vpts.iloc[0, :].to_dict().values()])
 
     def test_column_order(self, vpts_version, path_with_vp):
@@ -89,15 +91,33 @@ class TestVpts:
             by=list(vpts_spec.sort.keys())).astype(str)
         pd.testing.assert_frame_equal(df_pre, df_post)
 
-    def test_nodata(self, vpts_version):
+    def test_nodata(self, vpts_version, path_with_vp):
         """vpts nodata values are serialized correctly in the output"""
-        # TODO
-        assert True
+        file_path = sorted(path_with_vp.rglob("bewid*.h5"))[0]
+        vpts_spec = get_vpts_version(vpts_version)
+        with ODIMReader(file_path) as odim_vp:
+            bird_profile = BirdProfile.from_odim(odim_vp, file_path.name)
+            # raw data as saved in the hdf5 file
+            dd_raw = np.array(odim_vp.hdf5["dataset1"]["data2"]["data"]).flatten()  # data2 -> dd
+            dd_nodata = odim_vp.hdf5["dataset1"]["data2"]["what"].attrs["nodata"]
+        # values after conversion
+        dd_vpts = bird_profile.to_vp(vpts_spec)["dd"]
+        # nodata marked in vpts version need to be the same as nodata marked in raw hdf5 data
+        assert ((dd_vpts == vpts_spec.nodata) == (dd_raw == dd_nodata)).all()
 
-    def test_undetect(self, vpts_version):
+    def test_undetect(self, vpts_version, path_with_vp):
         """vpts undetect values are serialized correctly in the output"""
-        # TODO
-        assert True
+        file_path = sorted(path_with_vp.rglob("bejab*.h5"))[0]
+        vpts_spec = get_vpts_version(vpts_version)
+        with ODIMReader(file_path) as odim_vp:
+            bird_profile = BirdProfile.from_odim(odim_vp, file_path.name)
+            # raw data as saved in the hdf5 file
+            ff_raw = np.array(odim_vp.hdf5["dataset1"]["data1"]["data"]).flatten()  # data1 -> ff
+            ff_undetect = odim_vp.hdf5["dataset1"]["data1"]["what"].attrs["undetect"]
+        # values after conversion
+        ff_vpts = bird_profile.to_vp(vpts_spec)["ff"]
+        # undetect marked in vpts version need to be the same as undetect marked in raw hdf5 data
+        assert ((ff_vpts == vpts_spec.undetect) == (ff_raw == ff_undetect)).all()
 
     def test_heights_all_the_same(self, vpts_version, path_with_vp):
         """vpts data contains the same levels/heights for each timestamp/radar"""
@@ -119,14 +139,14 @@ class TestVpts:
             return dict(
                 radar=bird_profile.identifiers["NOD"],
                 datetime=datetime_to_proper8601(bird_profile.datetime),
-                vcp=int_to_nodata(vp_metadata_only.how["vcp"], ["NULL", 0],
+                vcp=int_to_nodata(str(vp_metadata_only.how["vcp"]), ["NULL", "0"],
                                   vpts_csv_version.nodata),
                 height=bird_profile.levels,
                 source_file=bird_profile.source_file
             )
         monkeypatch.setattr(vpts_csv_version, "mapping", _mock_mapping)
 
-        # bird profile (vp_metadata_only fixture) containing 0 values
+        # bird profile (vp_metadata_only fixture) containing 0 values (OdimReader reads as int)
         vp_metadata_only.how["vcp"] = 0
         df = vp_metadata_only.to_vp(vpts_csv_version)
         assert df["vcp"].unique() == vpts_csv_version.nodata
@@ -141,6 +161,25 @@ class TestVpts:
         df = vp_metadata_only.to_vp(vpts_csv_version)
         assert df["vcp"].unique() == np.array(['12'])
 
+    def test_vp_no_source_file(self, vpts_version, path_with_vp):
+        """The file name itself is used when no source_file reference is provided"""
+        file_path = sorted(path_with_vp.rglob("*.h5"))[0]
+        df_vp = vp(file_path, vpts_version)
+        assert set(df_vp["source_file"].unique()) == set( [file_path.name])
+        assert df_vp.reset_index(drop=True)["source_file"][0] == "bejab_vp_20221111T233000Z_0x9.h5"
+
+    def test_vp_custom_callable_file(self, vpts_version, path_with_vp):
+        """The source file reference can be overwritten by a custom callable using the
+        file_path as input"""
+        file_path = sorted(path_with_vp.rglob("*.h5"))[0]
+        # Use a function returning a dummy value for each
+        df_vp = vp(file_path, vpts_version, _convert_to_source_dummy)
+        assert (df_vp["source_file"] == "DUMMY VALUE").all()
+
+        # Use a conversion to s3 function
+        df_vp = vp(file_path, vpts_version, _convert_to_source_s3)
+        assert df_vp["source_file"].str.startswith("s3://aloft/baltrad").all()
+
     def test_vpts_no_source_file(self, vpts_version, path_with_vp):
         """The file name itself is used when no source_file reference is provided"""
         file_paths = sorted(path_with_vp.rglob("*.h5"))
@@ -148,8 +187,14 @@ class TestVpts:
         assert set(df_vpts["source_file"].unique()) == set(file_path.name for file_path in file_paths)
         assert df_vpts.reset_index(drop=True)["source_file"][0] == "bejab_vp_20221111T233000Z_0x9.h5"
 
-    def test_vpts_no_source_file(self, vpts_version, path_with_vp):
-        """The source°file reference can be overwritten by a custom callable using the file_path as input"""
+    def test_vpts_convert_to_source(self, vpts_version):
+        """Default helper function for filepath to file name returns name of path"""
+        fname = _convert_to_source(Path("./odimh5/bewid_pvol_20170214T0000Z_0x1.h5"))
+        assert fname == "bewid_pvol_20170214T0000Z_0x1.h5"
+
+    def test_vpts_custom_callable_file(self, vpts_version, path_with_vp):
+        """The source file reference can be overwritten by a custom callable using the
+        file_path as input"""
         file_paths = sorted(path_with_vp.rglob("*.h5"))
         # Use a function returning a dummy value for each
         df_vpts = vpts(file_paths, vpts_version, _convert_to_source_dummy)
@@ -158,6 +203,22 @@ class TestVpts:
         # Use a conversion to s3 function
         df_vpts = vpts(file_paths, vpts_version, _convert_to_source_s3)
         assert df_vpts["source_file"].str.startswith("s3://aloft/baltrad").all()
+
+    def test_vp_invalid_file(self, vpts_version, path_with_wrong_h5):  # noqa
+        """Invalid h5 vp file raises InvalidSourceODIM exceptin"""
+        with pytest.raises(InvalidSourceODIM):
+            vp(path_with_wrong_h5 / "dummy.h5")
+
+    def test_abstractmethoddefaults(self, vpts_version):
+        """Test properties of abstract base class level"""
+        AbstractVptsCsv.__abstractmethods__ = set()
+        class Dummy(AbstractVptsCsv):
+            """Dummn subclass"""
+        vpts_dummy = Dummy()
+        assert vpts_dummy.nodata == ""
+        assert vpts_dummy.undetect == "NaN"
+        assert vpts_dummy.sort == dict()
+        assert vpts_dummy.mapping(dict()) == dict()
 
 
 @pytest.mark.parametrize("vpts_version", ["v1.0"])
