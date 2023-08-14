1e6aa7630679c93ab908cd58a8ec8fccb17c8db0
peterdesmet <peterdesmet@users.noreply.github.com>
2023-08-14 13:16:47 +0000

    deploy: 59297672fb2c36379904ea68c1305402f45aa171

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post1.dev6+gb808fda',
+    VERSION: '0.1.0.post1.dev7+g5929767',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
