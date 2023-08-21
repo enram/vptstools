a3ad7455f040af62e50ac30d0e172754be5f9f25
stijnvanhoey <stijnvanhoey@users.noreply.github.com>
2023-08-21 22:55:51 +0000

    deploy: 6cb8577d9b4c643443457054075a30cd12995f3e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0.post1.dev12+gc4f7668',
+    VERSION: '0.2.0.post1.dev1+g6cb8577',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
